5c178da1aceb868ed1530332687e33fb263370a9
GabLopes12 <108309047+GabLopes12@users.noreply.github.com>
2023-10-20 13:17:21 -0300

    dominio.h

--- dominio.h
+++ dominio.h
@@ -11,10 +11,22 @@ class CODIGO{
     private:
         string codigo;
         void verifica(string);
+        /** 
+        * verifica se o código atende a critérios específicos.
+        * @param o código verificado.
+        * saída: "argumento inválido" caso o código não atenda a um dos críterios estabelecidos.
+        */
 
     public:
         string GetCodigo();
+        /**
+        * obtem o código;
+        * retorna esse código.
+        */
         void SetCodigo(string);
+        /**
+        * define o código.
+        */
 };
 
 //---------------------------------------------------------------------------//
@@ -23,10 +35,22 @@ class TEXTO{
     private:
         string texto;
         void verifica(string);
+        /**
+        * verifica se o texto atende a alguns critérios específicos.
+        * @param o texto verificado.
+        * saída: "argumento inválido" caso o texto não atenda a um dos críterios estabelecidos.
+        */
     
     public:
         string GetTexto();
+        /**
+        * obtem o texto;
+        * retorna esse código.
+        */
         void SetTexto(string);
+        /**
+        * define o texto.
+        */
 };
 
 //---------------------------------------------------------------------------//
@@ -35,10 +59,22 @@ class LIMITE{
     private:
         int limite;
         void verifica(int);
-
+        /**
+        * verifica se o limite atende a alguns critérios específicos.
+        * @param o limite verificado.
+        * saída: "argumento inválido" caso o limite não atenda a um dos críterios estabelecidos.
+        */
+        
     public:
         int GetLimite();
+        /**
+        * obtem o limite;
+        * retorna esse limite.
+        */
         void SetLimite(int);
+        /**
+        * define o limite.
+        */
 };
 
 //---------------------------------------------------------------------------//
@@ -47,10 +83,22 @@ class COLUNA{
     private:
         string coluna;
         void verifica(string);
+        /** 
+        * verifica se a coluna atende a critérios específicos.
+        * @param a coluna verificada.
+        * saída: "argumento inválido" caso a coluna não atenda a um dos críterios estabelecidos.
+        */
         
     public:
         string GetColuna();
+        /**
+        * obtem a coluna;
+        * retorna essa coluna.
+        */
         void SetColuna(string);
+        /**
+        * define a coluna.
+        */
 };
 
 //---------------------------------------------------------------------------//
@@ -59,10 +107,22 @@ class SENHA{
     private:
         string senha;
         void verifica(string);
+        /** 
+        * verifica se a senha atende a critérios específicos.
+        * @param a senha verificada.
+        * saída: "argumento inválido" caso o código não atenda a um dos críterios estabelecidos.
+        */
         
     public:
         string GetSenha();
+        /**
+        * obtem a senha;
+        * retorna essa senha.
+        */
         void SetSenha(string);
+        /**
+        * define a senha.
+        */
 };
 
 //---------------------------------------------------------------------------//
@@ -71,10 +131,22 @@ class EMAIL{
     private:
         string email;
         void verifica(string);
+        /** 
+        * verifica se o endereço de email atende a critérios específicos.
+        * @param o endereço de email verificado.
+        * saída: "argumento inválido" caso o endereço de email não atenda a um dos críterios estabelecidos.
+        */
         string SeparaEmail(string, string);
     public:
         void SetEmail(string);
+        /**
+        * obtem o endereço de email;
+        * retorna esse endereço de email.
+        */
         string GetEmail();
+        /**
+        * define o endereço de email.
+        */
 };
 
 //---------------------------------------------------------------------------//
